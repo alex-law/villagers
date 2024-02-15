ac917205c4248fc16098a5b80d47388b8043e311
Alex Law <alexlawcodes@gmail.com>
2024-02-15 21:10:09 +0000

    add players and set up lobby

--- main.py
+++ main.py
@@ -28,30 +28,41 @@ def newGame():
     game_id = request.form.get("game_id")
     new_game = request.form.get("new_game")
     if (len(game_id.strip()) == 0 and new_game is None):
-        # return redirect(url_for("home"), valid='Please select new game or enter a game id')
         return render_template("home.html", valid= 'Please select new game or enter a game id')
-    #If game id is not valid tell user
-    if (db.collection.find_one({'game_id': game_id}) is None and new_game is None):
-        return render_template("home.html", valid='Game id not valid, please check and enter again')
+    #If game id already exists and user is trying to start a new game
+    if (db.collection.find_one({'game_id': game_id}) is not None) and (new_game is not None):
+        return render_template("home.html", valid='Game id is currently in use, please choose a new id')
+    #If game id does not exist and user is trying to join game
+    if (db.collection.find_one({'game_id': game_id}) is None) and (new_game is None):
+        return render_template("home.html", valid='Game id does not exist, please check your game id or request a new one')
     #Made it past all this so initiate player
     player = playerObj(player_name)
     #Start a new game
     if new_game is not None:
         #Initiate game obj
-        game = gameObj(player)
+        game = gameObj(player, init_game_id=game_id)
         #save game to db
-        game.game_id = db.collection.insert_one(game.toDict()).inserted_id
+        game.mongo_id = db.collection.insert_one(game.gameToDict()).inserted_id
         return render_template('lobby.html', game=game)
     #Otherwise get current game
     if new_game is None:
         #Get existing game
-        game_db = db.collection.get_one({'game_id': game_id})
+        game_db = db.collection.find_one({'game_id': game_id})
+        #Check to make sure that player doesn't already exist
+        curr_players = game_db['players']
+        if player.player_name in [p['player_name'] for p in curr_players]:
+            return render_template("home.html", valid='Player name already taken, please pick a new one')
+        #Add new player
+        curr_players.append(player.toDict())
+        filter = {'_id': game_db['_id']}
+        update_fields = {'$set': {'players': curr_players}}
+        # Update the document
+        result = db.collection.update_one(filter, update_fields, upsert=False)
+
         #Get game obj
-        game = gameObj(player, game_db)
+        game = gameObj(player, game_db=game_db)
+        #Update game dictionary
         return render_template('lobby.html', game=game)
-
-    
-
 
 @app.get("/update/<int:todo_id>")
 def update(todo_id):
--- villageObjects.py
+++ villageObjects.py
@@ -1,21 +1,28 @@
 class gameObj:
 
-    def __init__(self, player, game_db=None):
+    def __init__(self, player, init_game_id='', game_db=None):
         if game_db is None:
             self.players = [player]
             self.game_state = 'initialised'
+            self.game_id = init_game_id
         else:
             self.players = game_db['players']
             self.game_state = game_db['game_state']
-            self.game_id = game_db['_id']
+            self.game_id = game_db['game_id']
 
-    def toDict(self):
+    def gameToDict(self):
         """Convert obj to dict for loading to mongo db"""
-        db_dict = {'players': self.players,
-                   'game_state': self.game_state}
+        db_dict = {'players': self.playersToList(),
+                   'game_state': self.game_state,
+                   'game_id': self.game_id}
         return db_dict
     
-    def getPlayers(self)
+    def playersToList(self):
+        """Convert obj to dict for loading to mongo db"""
+        players_list = [p.toDict() for p in self.players]
+        return players_list
+    
+    def getPlayers(self):
         pass
 
     def assignCharacters(self):
@@ -26,3 +33,9 @@ class playerObj:
     def __init__(self, player_name):
         self.player_name = player_name
         self.character = ''
+
+    def toDict(self):
+        """Convert obj to dict for loading to mongo db"""
+        player_dict = {'player_name': self.player_name,
+                   'character': self.character}
+        return player_dict
