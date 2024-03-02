a961df11a305a3ed46bff6849ac2f284b99cf53e
Alex Law <alexlawcodes@gmail.com>
2024-03-02 16:57:45 +0000

    kubernetes running locally with minikube

--- usefull_commands.sh
+++ usefull_commands.sh
@@ -1,1 +1,9 @@
 docker-compose -f mongo_db/docker-compose.mongo_db.yml up -d
+
+
+& minikube -p minikube docker-env --shell powershell | Invoke-Expression
+minikube image load villagersflaskapp:latest
+
+kubectl get nodes -o wide
+
+minikube service flask-service
